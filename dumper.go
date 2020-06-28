74a43bcdb2f8d12c20bc83816db823d746cce087
Ronny Bangsund <github@neuralshortcircuit.com>
2020-06-28 19:12:51 +0200

    Simplified Dumper structure and renamed Register() to NewDumper().

--- dumper.go
+++ dumper.go
@@ -3,41 +3,25 @@ package mysqldump
 import (
 	"database/sql"
 	"errors"
-	"path"
+	"path/filepath"
 	"time"
 )
 
 // Dumper represents a database.
 type Dumper struct {
-	db       *sql.DB
-	basename string
-	dir      string
-	path     string
+	db   *sql.DB
+	path string
 }
 
-// Path returns the full path of the generated dump.
-func (d *Dumper) Path() string {
-	return d.path
-}
-
-/*
-	Register a database and return a new dumper.
-
-	db: Database that will be dumped (https://golang.org/pkg/database/sql/#DB).
-	dir: Path to the directory where the dumps will be stored.
-	basename: Stem to be used to name each dump file. Uses time.Time.Format (https://golang.org/pkg/time/#Time.Format). format.
-*/
-func Register(db *sql.DB, dir, basename string) (*Dumper, error) {
+func NewDumper(db *sql.DB, dir, basename string) (*Dumper, error) {
 	if !isDir(dir) {
 		return nil, errors.New("Invalid directory")
 	}
 
-	name := time.Now().Format(basename)
+	path := filepath.Join(dir, time.Now().Format(basename))
 	return &Dumper{
-		db:       db,
-		basename: basename,
-		dir:      dir,
-		path:     path.Join(dir, name),
+		db:   db,
+		path: path,
 	}, nil
 }
 
@@ -50,4 +34,9 @@ func (d *Dumper) Close() error {
 		d.db = nil
 	}()
 	return d.db.Close()
+}
+
+// Path returns the full path of the generated dump.
+func (d *Dumper) Path() string {
+	return d.path
 }
